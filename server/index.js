04e52a49db216e56a27e9089b435f57b6470f514
Ben Jacobs <ben.jacobs@vanti.co.uk>
2017-11-19 23:49:32 +0000

    add api methods for the remaining functions

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const NissanConnect = require('@beejjacobs/nissan-connect').NissanConnect;
 const NissanConnectStore = require('./src/nissan-connect-store');
 const config = require('./config-real.json');
@@ -8,6 +9,7 @@ let password = Buffer.from(config.password, 'base64').toString('ascii');
 const nissanConnect = new NissanConnect(config.username, password);
 const store = new NissanConnectStore(config.username, nissanConnect);
 
+app.use(bodyParser.json());
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -25,7 +27,50 @@ function failure(res, error) {
   res.send(error);
 }
 
-app.get('/drive/analysis', function (req, res) {
+app.get('/battery/status', function (req, res) {
+  nissanConnect.getBatteryStatus()
+      .then(data => success(res, data))
+      .catch(error => failure(res, error));
+});
+app.get('/battery/status/last', function (req, res) {
+  nissanConnect.getLastBatteryStatus()
+      .then(data => success(res, data))
+      .catch(error => failure(res, error));
+});
+
+app.get('/battery/charge', function (req, res) {
+  nissanConnect.startCharging()
+      .then(data => success(res, data))
+      .catch(error => failure(res, error));
+});
+
+app.get('/ac/on', function (req, res) {
+  nissanConnect.acOn()
+      .then(data => success(res, data))
+      .catch(error => failure(res, error));
+});
+app.get('/ac/off', function (req, res) {
+  nissanConnect.acOff()
+      .then(data => success(res, data))
+      .catch(error => failure(res, error));
+});
+app.get('/ac/schedule', function (req, res) {
+  nissanConnect.getAcSchedule()
+      .then(data => success(res, data))
+      .catch(error => failure(res, error));
+});
+app.get('/ac/schedule/:schedule', function (req, res) {
+  nissanConnect.setAcSchedule(req.params.schedule)
+      .then(data => success(res, data))
+      .catch(error => failure(res, error));
+});
+app.get('/ac/schedule/cancel', function (req, res) {
+  nissanConnect.cancelAcSchedule()
+      .then(data => success(res, data))
+      .catch(error => failure(res, error));
+});
+
+app.get('/drive/analysis/today', function (req, res) {
   nissanConnect.getDrivingAnalysisToday()
       .then(data => success(res, data))
       .catch(error => failure(res, error));
@@ -33,6 +78,12 @@ app.get('/drive/analysis', function (req, res) {
 
 app.get('/drive/analysis/week/:date', function (req, res) {
   store.getDrivingAnalysisWeek(req.params.date)
+      .then(data => success(res, data))
+      .catch(error => failure(res, error));
+});
+
+app.get('/drive/record/today', function (req, res) {
+  nissanConnect.getDriveRecordsToday()
       .then(data => success(res, data))
       .catch(error => failure(res, error));
 });
@@ -57,6 +108,12 @@ app.get('/drive/record/month/:month', function (req, res) {
 
 app.get('/drive/record/year/:year', function (req, res) {
   store.getDriveRecordsYear(req.params.year)
+      .then(data => success(res, data))
+      .catch(error => failure(res, error));
+});
+
+app.post('/drive/record/note/:date', function (req, res) {
+  nissanConnect.addDriveRecordNote(req.params.date, req.body.note)
       .then(data => success(res, data))
       .catch(error => failure(res, error));
 });
@@ -104,6 +161,12 @@ app.get('/drive/distance/time/year/:year', function (req, res) {
 });
 app.get('/drive/energy/year/:year', function (req, res) {
   store.getYearlyEnergyUsage(req.params.year)
+      .then(data => success(res, data))
+      .catch(error => failure(res, error));
+});
+
+app.get('/vehicle/info', function (req, res) {
+  nissanConnect.getVehicleInfo()
       .then(data => success(res, data))
       .catch(error => failure(res, error));
 });
