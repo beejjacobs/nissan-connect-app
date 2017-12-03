e228473779c3247233878833435fc989aa06b3cd
Ben Jacobs <ben.jacobs@vanti.co.uk>
2017-12-03 23:51:43 +0000

    change to app-drawer menu system

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,48 +1,119 @@
 <template>
   <v-app>
-    <v-toolbar app color="primary">
-      <v-toolbar-title class="white--text">{{ title }}</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn flat :to="{path:'/info'}" exact>
-        <v-icon left>info</v-icon>
-        <span>Info</span>
-      </v-btn>
-      <v-btn flat :to="{path:'/'}" exact>
-        <v-icon left>ev_station</v-icon>
-        <span>Battery</span>
-      </v-btn>
-      <v-btn flat :to="{path:'/ac'}">
-        <v-icon left>toys</v-icon>
-        <span>AC</span>
-      </v-btn>
-      <v-btn flat :to="{path:'/history'}">
-        <v-icon left>directions_car</v-icon>
-        <span>History</span>
-      </v-btn>
+    <v-navigation-drawer
+        fixed
+        app
+        clipped
+        v-model="drawer"
+    >
+      <v-list dense>
+        <template v-for="(item, i) in items">
+          <v-layout
+              row
+              v-if="item.heading"
+              align-center
+              :key="i"
+          >
+            <v-flex xs6>
+              <v-subheader v-if="item.heading">
+                {{ item.heading }}
+              </v-subheader>
+            </v-flex>
+            <v-flex xs6 class="text-xs-center">
+              <a href="#!" class="body-2 black--text">EDIT</a>
+            </v-flex>
+          </v-layout>
+          <v-list-group v-else-if="item.children" v-model="item.model" no-action>
+            <v-list-tile slot="item" @click="">
+              <v-list-tile-action>
+                <v-icon>{{ item.model ? item.icon : item['icon-alt'] }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ item.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-list-tile
+                v-for="(child, i) in item.children"
+                :key="i"
+                :to="child.link"
+                @click=""
+            >
+              <v-list-tile-action v-if="child.icon">
+                <v-icon>{{ child.icon }}</v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>
+                  {{ child.text }}
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list-group>
+          <v-list-tile v-else @click="" :to="item.link" exact>
+            <v-list-tile-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>
+                {{ item.text }}
+              </v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </template>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar
+        app
+        fixed
+        dark
+        clipped-left
+        color="primary"
+    >
+      <v-toolbar-title class="white--text">
+        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+        {{ title }}
+      </v-toolbar-title>
     </v-toolbar>
-    <main>
-      <v-content>
-        <v-container fluid>
-          <router-view></router-view>
-        </v-container>
-      </v-content>
-    </main>
+    <v-content>
+      <v-container fluid fill-height>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
   </v-app>
 </template>
 
 <script>
   export default {
     name: 'app',
+    data: () => ({
+      drawer: false,
+      items: [
+        { icon: 'info', text: 'Info', link: '/info' },
+        { icon: 'ev_station', text: 'Battery', link: '/' },
+        { icon: 'toys', text: 'AC', link: '/ac' },
+        {
+          icon: 'keyboard_arrow_up',
+          'icon-alt': 'keyboard_arrow_down',
+          text: 'History',
+          model: true,
+          link: '/history',
+          children: [
+            { icon: 'list', text: 'Summary', link: '/history' },
+            { icon: 'settings', text: 'Test', link: '/test' }
+          ]
+        }
+      ]
+    }),
     computed: {
       title: function () {
         return this.$route.meta.title;
       }
+    },
+    mounted() {
+      this.$router.afterEach((to, from) => {
+        this.drawer = false;
+      });
     }
   }
 </script>
-
-<style>
-  html {
-    overflow-y: auto;
-  }
-</style>
