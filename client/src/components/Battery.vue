2d2042041320e3e939b3582a85b85304259d703f
Ben Jacobs <ben.jacobs@vanti.co.uk>
2017-12-05 23:29:14 +0000

    add some styling to the battery page

--- client/src/components/Battery.vue
+++ client/src/components/Battery.vue
@@ -1,58 +1,80 @@
 <template>
-  <v-container>
-    <battery-level :height="batteryHeight"
-                   :width="batteryWidth"
-                   stroke-width="4"
-                   stroke-color="#808080"
-                   fill-color="#009688"
-                   :charging="isCharging"
-                   :level="status.chargeState"></battery-level>
-    <div style="float: right;">
-      <h4>Range: {{ status.range | mToMiles }} miles</h4>
-      <h4>with ac: {{ status.rangeWithAc | mToMiles }} miles</h4>
-      <h4>Charge level: {{status.chargeState | chargePercentage}}%</h4>
-      <h4 v-if="isCharging">Charging</h4>
-      <v-btn v-if="!isCharging && pluggedIn" @click="startCharge">Start Charging</v-btn>
-      <h4 v-if="pluggedIn">Plugged In</h4>
-      <h4 v-else="">Unplugged</h4>
-      <h5 v-if="isCharging">Time Left:</h5>
-      <table v-if="isCharging">
-        <tr v-if="status.timeToFull">
-          <td>Standard</td>
-          <td>{{status.timeToFull | timeToCharge}}</td>
-        </tr>
-        <tr v-if="status.timeToFull3kW">
-          <td>3kW</td>
-          <td>{{status.timeToFull3kW | timeToCharge}}</td>
-        </tr>
-        <tr v-if="status.timeToFull6kW">
-          <td>6kW</td>
-          <td>{{status.timeToFull6kW | timeToCharge}}</td>
-        </tr>
-      </table>
-    </div>
-    <div v-if="updateTime">Last updated: {{updateTime | calendar}}</div>
-    <div v-else-if="status.updateTime">Last updated: {{status.updateTime | calendar}}</div>
-    <v-btn
-        color="pink"
-        dark
-        small
-        bottom
-        right
-        fab
-        fixed
-        @click="refresh()"
-    >
-      <v-icon>refresh</v-icon>
-    </v-btn>
-  </v-container>
+  <v-card>
+    <v-card-title>
+      <v-spacer></v-spacer>
+      <v-btn
+          color="accent"
+          dark
+          fab
+          right
+          @click="refresh()"
+      >
+        <v-icon>refresh</v-icon>
+      </v-btn>
+    </v-card-title>
+    <v-card-text class="grid headline">
+      <div class="battery">
+        <battery-level
+            :height="batteryHeight"
+            :width="batteryWidth"
+            stroke-width="4"
+            stroke-color="#4c4c4c"
+            fill-color="#246899"
+            :charging="isCharging"
+            :level="status.chargeState"
+        ></battery-level>
+        <div class="body-1">Updated: {{updateTime ? updateTime : status.updateTime | calendar}}</div>
+      </div>
+      <div class="info-block range">
+        <v-btn color="accent" dark icon><v-icon>keyboard_tab</v-icon></v-btn>
+        {{ status.range | mToMiles }} miles
+      </div>
+      <div class="info-block range-ac">
+        <v-btn color="accent" dark icon><v-icon>toys</v-icon></v-btn>
+        {{ status.rangeWithAc | mToMiles }} miles
+      </div>
+      <div class="info-block charge">
+        <v-btn color="accent" dark icon><v-icon>battery_unknown</v-icon></v-btn>
+        {{status.chargeState | chargePercentage}}%
+      </div>
+      <div class="info-block charging">
+        <v-btn :color="isCharging ? 'accent' : 'secondary'" dark icon><v-icon>{{isCharging ? 'flash_on' : 'flash_off'}}</v-icon></v-btn>
+      </div>
+      <div class="info-block plugged-in">
+        <v-btn :color="pluggedIn ? 'accent' : 'secondary'" dark icon><v-icon>power</v-icon></v-btn>
+        {{pluggedIn ? 'Plugged In' : 'Unplugged'}}
+      </div>
+      <div class="time-left">
+        <v-btn color="accent" dark icon><v-icon>schedule</v-icon></v-btn>
+        <table>
+          <tr v-if="status.hasTimeToFull">
+            <td>2.3kW</td>
+            <td>{{status.timeToFull | timeToCharge}}</td>
+          </tr>
+          <tr v-if="status.hasTimeToFull3kW">
+            <td>3.3kW</td>
+            <td>{{status.timeToFull3kW | timeToCharge}}</td>
+          </tr>
+          <tr v-if="status.hasTimeToFull6kW">
+            <td>6.6kW</td>
+            <td>{{status.timeToFull6kW | timeToCharge}}</td>
+          </tr>
+        </table>
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" v-if="!isCharging && pluggedIn" @click="startCharge">Start Charging</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
   import moment from 'moment';
+
   export default {
     name: 'Battery',
-    data () {
+    data() {
       return {
         status: {
           range: 0,
@@ -75,10 +97,12 @@
     },
     computed: {
       pluggedIn() {
-        return this.status.pluginState !== 'NOT_CONNECTED';
+        return this.status.pluginState === 'QC_CONNECTED' ||
+            this.status.pluginState === 'CONNECTED';
       },
       isCharging() {
-        return this.status.isCharging === 'YES';
+        return this.status.chargeStatus === 'NORMAL_CHARGING' ||
+            this.status.chargeStatus === 'RAPIDLY_CHARGING';
       }
     },
     filters: {
@@ -124,4 +148,46 @@
 </script>
 
 <style scoped>
+  table td {
+    padding: 5px;
+  }
+  table td:last-child {
+    text-align: right;
+  }
+  .grid {
+    display: grid;
+    height: 100%;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+    grid-template-areas:
+        "bat rag ac"
+        "bat cha chg"
+        "bat plg tim";
+  }
+  .battery {
+    grid-area: bat;
+  }
+
+  .info-block {
+
+  }
+
+  .range {
+    grid-area: rag;
+  }
+  .range-ac {
+    grid-area: ac;
+  }
+  .charge {
+    grid-area: cha;
+  }
+  .charging {
+    grid-area: chg;
+  }
+  .plugged-in {
+    grid-area: plg;
+  }
+  .time-left {
+    grid-area: tim;
+  }
 </style>
