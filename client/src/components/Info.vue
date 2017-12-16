3fbcaf905c9f89d5d30610d00f45732494473456
Ben Jacobs <ben.jacobs@vanti.co.uk>
2017-12-16 20:56:59 +0000

    basic info styling

--- client/src/components/Info.vue
+++ client/src/components/Info.vue
@@ -1,11 +1,20 @@
 <template>
-  <v-layout row wrap>
-    <vehicle-info
-      :fetch="false"
-      :data="vehicleInfo"
-    ></vehicle-info>
-    <customer-info></customer-info>
-  </v-layout>
+  <v-container
+      fluid
+      grid-list-lg
+  >
+    <v-layout row wrap>
+      <v-flex xs12>
+        <vehicle-info
+            :fetch="false"
+            :data="vehicleInfo"
+        ></vehicle-info>
+      </v-flex>
+      <v-flex xs12>
+        <customer-info></customer-info>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
